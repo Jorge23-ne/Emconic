de2003eff5f547902c1af0faec9d3a2d73cd12eb
Jorge23-ne <jorgegabo752@gmail.com>
2025-08-14 09:28:27 -0600

    Update Registro_login.jsx

--- Emconic/src/Registro_login.jsx
+++ Emconic/src/Registro_login.jsx
@@ -1,43 +1,50 @@
 import React from "react";
 import './ingresar_css.css';
 
-function Registrarse(){
+function Registrarse() {
 
-    return(
+    return (
 
         <>
-          <div className="Login">
-        <h2>
-            Ingresar
-        </h2>
-        <form className="Nombre" action="">
-          
-            <input type="text" placeholder="Nombre de usuario"></input>
 
-        </form>
-         
-        <div className="Correo">
+            <div className="Login">
+                <div class="Tilo"></div>
 
-           
-            <input type="text" name="" id="" placeholder="Correo electrónico"></input>
-            
-        </div>
-        <div className="Telefono">
 
-           
-            <input type="text" name="" id="" placeholder="Número de teléfono"></input>
-            
-        </div>
-        
-        <div className="Contraseña">
 
-            
-            <input type="password" name="" id="" placeholder="Contraseña"></input>
-            
-        </div>
-        <button className="Boton" type="submit">Enviar</button>
 
-    </div>
+                <h2>
+                    Ingresar
+                </h2>
+                <form className="Nombre1" action="">
+
+                    <input type="text" placeholder="Nombre de usuario"></input>
+
+                </form>
+
+                <div className="Correo1">
+
+
+                    <input type="text" name="" id="" placeholder="Correo electrónico"></input>
+
+                </div>
+                <div className="Telefono1">
+
+
+                    <input type="text" name="" id="" placeholder="Número de teléfono"></input>
+
+                </div>
+
+                <div className="Contraseña1">
+
+
+                    <input type="password" name="" id="" placeholder="Contraseña"></input>
+
+                </div>
+
+                <button className="Boton" type="submit">Enviar</button>
+
+            </div>
         </>
     )
 }
