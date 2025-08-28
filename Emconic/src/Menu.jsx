7ccaefb949a866974cb0af0e06ff314baf8a89d8
Jorge23-ne <jorgegabo752@gmail.com>
2025-08-28 08:10:39 -0600

    Update Menu.jsx

--- Emconic/src/Menu.jsx
+++ Emconic/src/Menu.jsx
@@ -1,12 +1,39 @@
-import React from "react";
-
+import React from "react"
+import "./Menu.css";
 function Menu() {
   return (
+
     <>
-      <header>
-        
-      </header>
+
+
+
+      <body className="Arriba">
+
+
+        <div className="Menu"> </div>
+        <div className="Img"></div>
+        <div className="Menu3">
+          <button className="D1" type="button">Ingresar Datos</button>
+          <button className="D2" type="button">Inventario</button>
+          <button className="D3" type="button">Estadisticas</button>
+          <button className="D4" type="button">Estado resultado</button>
+          <button className="D5" type="button">Reportes</button>
+          <button className="D6" type="button">Guia instruccional</button>
+        </div>
+
+
+
+        <div className="Menu1"></div>
+
+        <div className="Menu2"></div>
+
+
+      </body>
+
     </>
   )
+
+
 }
+export default Menu;
 
