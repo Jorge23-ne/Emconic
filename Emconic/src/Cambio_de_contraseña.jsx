ec1d5f9c5ab1c07928dcbc390d97708806d6ab6d
Jorge23-ne <jorgegabo752@gmail.com>
2025-08-28 09:10:28 -0600

    Create Cambio_de_contraseña.jsx

---
+++ Emconic/src/Cambio_de_contraseña.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Cambio_de_contraseña.css";
+
+function Cambio_de_contraseña() {
+
+    return (
+        <body className="cuerpo">
+            <div className="Cambio">
+                <h1 className="Cambio1">Cambio de contraeña</h1>
+
+                  <div className="G2">
+
+
+                    <input  type="password" name="" id="" placeholder="Ingrese la nueva contraseña"></input>
+
+                </div>
+                  <div className="G1">
+
+
+                    <input  type="password" name="" id="" placeholder="Confirme la nueva contraseña"></input>
+
+                </div>
+                <button className="G3" type="reset">Enviar</button>
+            </div>
+           
+            
+            
+   
+
+            
+
+
+
+
+        </body>
+
+    )
+}
+
+export default Cambio_de_contraseña;
