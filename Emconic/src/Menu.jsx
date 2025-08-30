85ca4639fc98627ad7265b4fb93dd1cfe3be5c1b
Jorge23-ne <jorgegabo752@gmail.com>
2025-08-30 13:18:03 -0600

    Update Menu.jsx

--- Emconic/src/Menu.jsx
+++ Emconic/src/Menu.jsx
@@ -1,39 +1,82 @@
-import React from "react"
+import React, { useState } from "react";
 import "./Menu.css";
+
 function Menu() {
+  const [info, setInfo] = useState(null);
+
   return (
-
     <>
-
-
-
-      <body className="Arriba">
-
-
-        <div className="Menu"> </div>
+     
+      <div className="Arriba">
+        <div className="Menu"></div>
         <div className="Img"></div>
+
         <div className="Menu3">
-          <button className="D1" type="button">Ingresar Datos</button>
-          <button className="D2" type="button">Inventario</button>
-          <button className="D3" type="button">Estadisticas</button>
-          <button className="D4" type="button">Estado resultado</button>
-          <button className="D5" type="button">Reportes</button>
-          <button className="D6" type="button">Guia instruccional</button>
+          <button
+            className="D1"
+            type="button"
+            onMouseEnter={() => setInfo("Aquí puedes ingresar nuevos datos")}
+            onMouseLeave={() => setInfo(null)}
+          >
+            Ingresar Datos
+          </button>
+
+          <button
+            className="D2"
+            type="button"
+            onMouseEnter={() => setInfo("Consulta o modifica el inventario")}
+            onMouseLeave={() => setInfo(null)}
+          >
+            Inventario
+          </button>
+
+          <button
+            className="D3"
+            type="button"
+            onMouseEnter={() => setInfo("Visualiza estadísticas")}
+            onMouseLeave={() => setInfo(null)}
+          >
+            Estadísticas
+          </button>
+
+          <button
+            className="D4"
+            type="button"
+            onMouseEnter={() => setInfo("Estado de resultados financieros")}
+            onMouseLeave={() => setInfo(null)}
+          >
+            Estado resultado
+          </button>
+
+          <button
+            className="D5"
+            type="button"
+            onMouseEnter={() => setInfo("Genera reportes personalizados")}
+            onMouseLeave={() => setInfo(null)}
+          >
+            Reportes
+          </button>
+
+          <button
+            className="D6"
+            type="button"
+            onMouseEnter={() => setInfo("Guía para comprender los procesos contables")}
+            onMouseLeave={() => setInfo(null)}
+          >
+            Guía instruccional
+          </button>
         </div>
 
-
+       
+        <div className="InfoBox">
+          {info && <p>{info}</p>}
+        </div>
 
         <div className="Menu1"></div>
-
         <div className="Menu2"></div>
-
-
-      </body>
-
+      </div>
     </>
-  )
-
-
+  );
 }
-export default Menu;
 
+export default Menu;
