927b6fc1536f62f1b272e05dbf08381d78600d42
Jorge23-ne <jorgegabo752@gmail.com>
2025-10-01 12:04:34 -0600

    Update Bienvenida.jsx

--- Emconic/src/Bienvenida.jsx
+++ Emconic/src/Bienvenida.jsx
@@ -13,37 +13,47 @@ function Bienvenida() {
 
 
 
-             
 
-            <div className="bienvenida">
-                
-            <div  class="elipse"></div>
-            
-              <div class="elipse1"></div>
-                
-                <div class="elipse2"></div>
-                
-                <div class="elipse3"></div>
-
-
-                <div class="contenedor"></div>
+            <main className="bienvenida1">
 
 
                 <h1 className="titulo">
                     Bienvenidos a Emconic
                 </h1>
 
-              
 
-                <h10 className="texto">
-                    Bienvenidos a la pagina EMCONIC, donde podras llevar las finanzas y cuentas de tu negocio de la manera mas facil y sencilla posible. Podras llevar conceptos de contabilidad complejos de una manera sencilla y entendible, sin necesidad de haber estudiado una carrera.
+                <div class="Estilo-contenedor">
+                    <p className="texto">
+                        Bienvenidos a la pagina EMCONIC, donde podras llevar las finanzas y cuentas de tu negocio de la manera mas facil y sencilla posible. Podras llevar conceptos de contabilidad complejos de una manera sencilla y entendible, sin necesidad de haber estudiado una carrera.
 
-                </h10>
+                    </p>
+                </div>
+
 
                 <Link to='/Login ' className="boton">Next</Link>
+            </main>
 
 
-            </div>
+            <div class="elipse"></div>
+
+            <div class="elipse1"></div>
+
+            <div class="elipse2"></div>
+
+            <div class="elipse3"></div>
+
+
+            <div class="contenedor"></div>
+
+
+
+            {/* <Link to='/Confirmacion' className="boton">Confirmacion</Link>
+               <Link to='/menu' className="boton">Menu</Link>
+                <Link to='/videos' className="boton">videos</Link>
+                <Link to='/balance_general' className="boton">Balance General</Link> */}
+
+
+
 
         </>
     );
