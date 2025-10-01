7031ec121ed412a8dea2ee5d96fe1a9b077b89d7
Jorge23-ne <jorgegabo752@gmail.com>
2025-10-01 11:09:12 -0600

    Update Balance_General.jsx

--- Emconic/src/Balance_General.jsx
+++ Emconic/src/Balance_General.jsx
@@ -1,32 +1,33 @@
 import React from "react";
-import "./Balance_General.css";
+import "./Menu.css";
 
-function Balance_General() {
-    return (
-        <div className="Balance_General">
-            <div className="Franja_Superior">
-                <h1 className="Titulo">Balance General</h1> 
-            </div>
+function Menu() {
 
-            <div className="Ubicacion">
-                {/* <button className="Activos">Activos</button>
-                <button className="Total_Activos" type="button">Total Activos</button>
-                <button className="Pasivos">Pasivos</button>
-                <button className="Total_Pasivos" type="button">Total Pasivos</button>
-                <button className="Patrimonio" type="button">Patrimonio</button> */}
-            </div>
 
-            <div className="Franja_Left">
-                <div className="contenedor-botones">
-                    <button className="Activos" type="button">Activos</button>
-                    <button className="Pasivos" type="button">Pasivos</button>
-                    <button className="Total_Activos" type="button">Total Activos</button>
-                    <button className="Total_Pasivos" type="button">Total Pasivos</button>
-                    <button className="Patrimonio" type="button">Patrimonio</button>
-                </div>
-            </div>
+  return (
+    <>
+
+      <div className="Balance_General">
+        <div className="Franja_Superior">
+          <h1 className="Titulo1">EMCONIC</h1>
         </div>
-    )
+
+        <div className="Franja_Left">
+          <div className="contenedor-botones">
+            <button className="Activos" type="button">Ingresar Datos</button>
+            <button className="Pasivos" type="button">Inventario</button>
+            <button className="Total_Activos" type="button">Estado resultado</button>
+            <button className="Total_Pasivos" type="button">Guia instruccional</button>
+            <button className="Patrimonio" type="button">Reportes</button>
+                 <button className="Patrimonio" type="button">Estadisticas</button>
+          </div>
+        </div>
+      </div>
+
+    </>
+
+
+  );
 }
 
-export default Balance_General;
+export default Menu;
